047b6cef28db771c26473ed871e8468ba248541e
Frank <135646005+frankyates06@users.noreply.github.com>
2024-03-02 23:07:52 +0100

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Title of the application
-st.title('Hello, World! Application')
+st.title('Hello, World! Application frank')
 
 # Display a simple message
 st.write('This is a simple Streamlit application saying "Hello, World!"')
